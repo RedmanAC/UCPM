0d8294add7546041a27f325554b2f922dc459f09
zekevo <zekevo@outlook.de>
2019-05-05 15:16:36 +0200

    Test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,7 +38,7 @@
 
                         @else
                          <li class="nav-item">
-                                    <a class="nav-link" href="/home">Home</a>
+                                    <a class="nav-link" href="/home">Homee</a>
                          </li>
                         @endguest
 
